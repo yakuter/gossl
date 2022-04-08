17f2af3ce53b3e3776d2dc4c1e4d891b4ff88f00
Erhan Yakut <yakuter@gmail.com>
2022-04-08 22:18:21 +0300

    refacto cert

--- commands/cert/cert.go
+++ commands/cert/cert.go
@@ -2,10 +2,18 @@ package cert
 
 import (
 	"bufio"
+	"bytes"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
 	"crypto/x509/pkix"
+	"encoding/pem"
 	"fmt"
 	"log"
+	"math/big"
+	"net"
 	"os"
+	"time"
 
 	"github.com/pkg/errors"
 	"github.com/urfave/cli/v2"
@@ -17,10 +25,11 @@ const (
 )
 
 const (
-	flagKey  = "key"
-	flagOut  = "out"
-	flagDays = "days"
-	flagIsCA = "isCA"
+	flagKey    = "key"
+	flagOut    = "out"
+	flagDays   = "days"
+	flagSerial = "serial"
+	flagIsCA   = "isCA"
 )
 
 func Command() *cli.Command {
@@ -52,56 +61,110 @@ func Flags() []cli.Flag {
 			Usage:    "Number of days a certificate is valid for",
 			Required: true,
 		},
+		&cli.Uint64Flag{
+			Name:     flagSerial,
+			Usage:    "Serial number to use in certificate",
+			Required: false,
+		},
 		&cli.BoolFlag{
 			Name:     flagIsCA,
-			Usage:    "Number of days a certificate is valid for",
+			Usage:    "Number of days the certificate is valid for",
 			Required: false,
 		},
 	}
 }
 
 func Action(c *cli.Context) error {
-	log.Printf("Certificate command args: %q\n", c.Args().Slice())
-
-	answers, err := readInputs()
-	if err != nil {
-		return errors.Wrapf(err, "failed to read inputs")
+	questions := []string{
+		"Common Name - SAN (eg, FQDN or IP)* []",
+		"Country Name (2 letter code) [AU]",
+		"State or Province Name []",
+		"Locality Name (eg, city) []",
+		"Organization Name [eg, company]",
+		"Organizational Unit Name (eg, section) []",
+		"Street Addr []",
+		"Postal Code []",
 	}
 
-	result := setPkixName(answers)
+	// Ask questions to user and get answers
+	answers, err := readInputs(questions)
+	if err != nil {
+		return fmt.Errorf("failed to read inputs %v", err)
+	}
 
-	log.Printf("%q", result)
+	if len(answers[0]) == 0 {
+		return errors.New("Common Name - SAN cannot be empty")
+	}
+
+	// Generate subject (pkix.Name) from answers
+	p := subject(answers)
+
+	// Generate template (x509 certificate)
+	t := template(p, c.Uint(flagDays), c.Uint64(flagSerial), c.Bool(flagIsCA))
+
+	// Get privatekey from file
+	privateKey, err := key(c.String(flagKey))
+	if err != nil {
+		return fmt.Errorf("Failed to get key from key file %s error: %v", c.String(flagKey), err)
+	}
+
+	// Create x509 certificate
+	certBytes, err := x509.CreateCertificate(rand.Reader, t, t, &privateKey.PublicKey, privateKey)
+	if err != nil {
+		return fmt.Errorf("Failed to create certificate error: %v", err)
+	}
+
+	// Encode certificate as PEM
+	certPEM := bytes.NewBuffer(nil)
+	err = pem.Encode(certPEM, &pem.Block{
+		Type:  "CERTIFICATE",
+		Bytes: certBytes,
+	})
+	if err != nil {
+		return fmt.Errorf("Failed to encode cert as pem error: %v", err)
+	}
+
+	// Set output
+	var output *os.File = os.Stdout
+
+	// If output file is provided, then create it and set as output
+	if c.IsSet(flagOut) {
+		outputFilePath := c.String(flagOut)
+		outputFile, err := os.Create(outputFilePath)
+		if err != nil {
+			return errors.Wrapf(err, "failed to create output file %q error", outputFilePath)
+		}
+
+		defer func() {
+			if err := outputFile.Close(); err != nil {
+				log.Printf("failed to close output file %q error %v", outputFilePath, err)
+			}
+		}()
+
+		output = outputFile
+	}
+
+	// Write PEM encoded x509 certificate to output
+	_, err = output.WriteString(certPEM.String())
+	if err != nil {
+		return errors.Wrapf(err, "failed to write output error")
+	}
 
 	log.Printf("Certificate generated successfully")
 	return nil
 }
 
-func readInputs() ([]string, error) {
-	answers := make([]string, 8)
-	questions := []string{
-		"Common Name - SAN (eg, server FQDN or IP) []: ",
-		"Country Name (2 letter code) [AU]: ",
-		"State or Province Name [Some-State]: ",
-		"Locality Name (eg, city) []: ",
-		"Organization Name [Internet Widgits Pty Ltd]: ",
-		"Organizational Unit Name (eg, section) []: ",
-		"Street Addr []: ",
-		"Postal Code []: ",
-	}
-
+func readInputs(questions []string) ([]string, error) {
+	answers := make([]string, len(questions))
 	scanner := bufio.NewScanner(os.Stdin)
 	for i := range questions {
-		fmt.Print(questions[i])
-		scanner.Scan()
-		text := scanner.Text()
-		if i == 0 && len(text) == 0 {
-			err := errors.New("Common Name - SAN cannot be empty")
-			return nil, errors.Wrapf(err, "error ")
+		fmt.Printf("%s: ", questions[i])
+		if ok := scanner.Scan(); !ok {
+			return nil, errors.Wrapf(errors.New("failed to scan"), "")
 		}
-		answers[i] = text
+		answers[i] = scanner.Text()
 	}
 
-	// handle error
 	if scanner.Err() != nil {
 		fmt.Println("Error: ", scanner.Err())
 	}
@@ -109,7 +172,7 @@ func readInputs() ([]string, error) {
 	return answers, nil
 }
 
-func setPkixName(answers []string) pkix.Name {
+func subject(answers []string) pkix.Name {
 	return pkix.Name{
 		Country:            []string{answers[1]},
 		Province:           []string{answers[2]},
@@ -119,4 +182,41 @@ func setPkixName(answers []string) pkix.Name {
 		StreetAddress:      []string{answers[6]},
 		PostalCode:         []string{answers[7]},
 	}
+}
+
+func template(subject pkix.Name, days uint, serial uint64, isCA bool) *x509.Certificate {
+	t := &x509.Certificate{
+		SerialNumber: big.NewInt(int64(serial)),
+		Subject:      subject,
+		NotBefore:    time.Now(),
+		NotAfter:     time.Now().AddDate(0, 0, int(days)),
+		ExtKeyUsage:  []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth, x509.ExtKeyUsageServerAuth},
+		KeyUsage:     x509.KeyUsageDigitalSignature | x509.KeyUsageCertSign,
+		IPAddresses:  []net.IP{net.IPv4(127, 0, 0, 1), net.IPv6loopback},
+	}
+
+	if isCA {
+		t.IsCA = true
+		t.BasicConstraintsValid = true
+	}
+	return t
+}
+
+func key(keyFilePath string) (*rsa.PrivateKey, error) {
+	keyFileContent, err := os.ReadFile(keyFilePath)
+	if err != nil {
+		return nil, err
+	}
+
+	keyBlock, _ := pem.Decode(keyFileContent)
+	if keyBlock == nil {
+		return nil, fmt.Errorf("invalid key file %s", keyFilePath)
+	}
+
+	key, err := x509.ParsePKCS1PrivateKey(keyBlock.Bytes)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse key file as x509 PKCS1 %v", err)
+	}
+
+	return key, nil
 }
